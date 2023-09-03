8dd6f1d00bc92910f5b028c7a7a3298469f839f2
Dagmara Grabowska <dagmarag0@gmail.com>
2023-09-03 15:08:43 +0200

    Initializated canvass with the ball class

--- visualizer.js
+++ visualizer.js
@@ -1,5 +1,37 @@
 const microphone = new Microphone()
 
+const canvas = document.getElementById("canvas1")
+const ctx = canvas.getContext("2d")
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Ball {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = 8;
+        this.color = "white";
+        this.jumpForce = 0;
+        this.fallForce = 0.1;
+
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill()
+    }
+
+    fall() {
+
+    }
+
+    jump() {
+        
+    }
+}
+
 function animate() {
     if (microphone.initialized) {
         const samples = microphone.getSamples();
